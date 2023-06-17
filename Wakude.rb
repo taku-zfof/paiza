fe75bb2052e765f8656b56eca50d48f8475d68c7
taku-zfof <taku.uchida.zfof@gmail.com>
2023-06-17 23:59:59 +0900

    [finish]C084

---
+++ Wakude.rb
@@ -0,0 +1,10 @@
+# 自分の得意な言語で
+# Let's チャレンジ！！
+input_line = gets.to_s.chomp
+line_length = input_line.length + 2
+
+puts "+" * line_length
+puts "+" + input_line + "+"
+puts "+" * line_length
+
+#入力した文字列を枠で囲む
